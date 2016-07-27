482eb92d2d03651519a91d9033a953dbe6cf2f7c
rafanetow <rafanetow@gmail.com>
2016-07-27 13:49:18 -0600

    begining to use refeltion

---
+++ MVCBase/Models/Object/ObjectModal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBase.Models.Object
+{
+    public class ObjectModal
+    {
+        private List<object> FieldsData { get; set; }
+        private List<object> FieldName { get; set; }
+
+    }
+}
